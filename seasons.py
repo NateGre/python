796d40a06d1d5349ae26073e11721cf242aecd6e
NateGre <124693183+NateGre@users.noreply.github.com>
2023-08-11 11:27:07 -0600

    Update seasons.py
    Fixed lists of ranges
    removed month * 100
    added lists of months with the same date numbers to error check
    made it work

--- seasons.py
+++ seasons.py
@@ -2,13 +2,23 @@ input_month = input()
 input_day = int(input())
 
 #season number ranges
-winter = [(1221,1231), (101,131), (201,228), (301,319)]
-spring = [(321,331), (401,430), (501,531), (601,620)]
-summer = [(621,631), (701,731), (801,831), (901,921)]
-autumn = [(922,930), (1001,1031), (1101,1130), (1201,1220)]
+winter = list(range(1221, 1232)) + list(range(101, 319))
+spring = range(320,621)
+summer = range(621,922)
+autumn = range(922,1221)
+
+#31 day months
+month_day_31 = [100, 300, 500, 700, 800, 1000, 1200]
+#30 day months
+month_day_30 = [400, 600, 900, 1100]
 
 #declare months = int
-months = {'January': 1, 'February':2, 'March':3, 'April':4, 'May':5, 'June':6, 'July':7, 'August':8, 'September':9, 'October':10, 'November':11, 'December':12}
+months = {
+        'January': 100, 'February':200, 'March':300,
+        'April':400, 'May':500, 'June':600, 
+        'July':700, 'August':800, 'September':900, 
+        'October':1000, 'November':1100, 'December':1200
+        }
 
 #search for input_month value in months dic
 key_val = [val for key, val in months.items() if input_month in key]
@@ -16,28 +26,35 @@ key_val = [val for key, val in months.items() if input_month in key]
 #error check
 if not key_val:
     print("Invalid")
-
-if input_day > 31:
-    print("Invalid")
+    exit()
 
 num = 0
+
 for month in key_val:
     num = num*10 + month
-    month = month * 100
-    date = int(month + input_day)
 
-if date in winter:
-    print(date,"Winter")
+date = int(month + input_day)
 
-if date in spring:
+if month in month_day_31 and input_day > 31:
+    print("Invalid")
+
+elif month in month_day_30 and input_day > 30:
+    print("Invalid")
+
+elif month == 200 and input_day > 28:
+    print("Invalid")
+   
+elif date in winter:
+    print("Winter")
+
+elif date in spring:
     print("Spring")
 
-if date in summer:
+elif date in summer:
     print("Summer")
 
-if date in autumn:
+elif date in autumn:
     print("Autumn")
     
 else:
     print("Invalid")
-
